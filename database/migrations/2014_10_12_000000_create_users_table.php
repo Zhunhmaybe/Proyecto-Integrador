d5abcae11985a45ffc878c83a5d03a9fd20cd282
Oskar Jurado <oskar@jurado.com>
2025-12-29 22:19:46 -0500

    arreglado migrate usuarios tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,9 @@ return new class extends Migration
             $table->string('password');
             $table->integer('rol')->default(3)->comment('1=Administrador, 2=Operador, 3=Usuario');
             $table->integer('estado')->default(1)->comment('1=Activo, 2=Inactivo');
+            $table->integer('failed_attempts')->default(0);
+            $table->integer('is_locked')->default(0);
+            $table->string('lock_code', 10)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
